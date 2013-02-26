a44474bf8a3b85be8e4a86b49325e09d0a652f4a
willonboy <willonboy@qq.com>
2013-02-26 15:29:55 +0800

    remove added target

---
+++ TwitterDemo/WZTwitterSDKLibDemo/WZTwitter.h
@@ -0,0 +1,74 @@
+//
+//  WZTwitter.h
+//  TwitterDemo
+//
+//  Created by willonboy zhang on 12-6-26.
+//  Copyright (c) 2012年 willonboy.tk. All rights reserved.
+//
+
+
+
+#import <Foundation/Foundation.h>
+#import "WZTWConfig.h"
+#import "WZTWRequest.h"
+#import "WZTWUtility.h"
+#import <UIKit/UIKit.h>
+
+
+@protocol WZTwitterDelegate;
+
+@interface WZTwitter : NSObject<UIWebViewDelegate, WZTWRequestDelegate>
+
+@property (nonatomic, assign) id<WZTwitterDelegate>    delegate;
+@property (nonatomic, retain) UIWebView             *webview ;
+
+- (BOOL)isLogin;
+
+- (void)authorization:(UIWebView *) webview_ ;
+
+- (void)logout;
+
+- (void)getUserInfo:(NSString *)userId userScreenName:(NSString *) userScreenName;;
+
+- (void)publishNewPhoto:(NSString *)content imgPath:(NSString *) shareImgPath;
+
+- (void)publishNewStatus:(NSString *)content ;
+
+- (void)getFriendInfoList:(NSString *)userId;
+
+
+
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+@protocol WZTwitterDelegate <NSObject>
+
+- (void)didLoginSuccessOrFailed:(BOOL) isSuccess;
+
+- (void)didLogoutSuccessOrFailed:(BOOL) isSuccess;
+
+- (void)receivedResponseData:(WZTwitter *)sender responseData:(NSString *)data apiMethod:(NSString *) apiMethod;
+
+- (void)requestFailed:(WZTwitter *)sender error:(NSError *)err apiMethod:(NSString *) apiMethod;
+
+
+@end
+
+
+
+
+
+
+
